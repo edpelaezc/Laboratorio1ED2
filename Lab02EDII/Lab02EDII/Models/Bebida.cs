cfc8fff513b3af4fe032d390f5c930a2fbf7256e
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-01-27 14:58:36 -0600

    Controlador de bebida

---
+++ Lab02EDII/Lab02EDII/Models/Bebida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab02EDII.Models
+{
+    public class Bebida
+    {
+        public int nombre { get; set; }
+        public string sabor { get; set; }
+        public string volumen { get; set; }
+        public int precio { get; set; }
+        public int casaProductora { get; set; }
+    }
+}
