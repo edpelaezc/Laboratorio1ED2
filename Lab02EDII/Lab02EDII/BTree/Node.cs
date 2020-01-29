dbba804350481dc037356260b8c920670d88dfca
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-01-29 09:00:20 -0600

    Método para comparar elemntos genéricos.

---
+++ Lab02EDII/Lab02EDII/BTree/Node.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab02EDII.Models;
+
+namespace Lab02EDII.BTree
+{
+    public class stringComparer : IComparer
+    {
+        // Call CaseInsensitiveComparer.Compare with the parameters reversed.
+        public int Compare(Object x, Object y)
+        {
+            return (new CaseInsensitiveComparer()).Compare(y, x);
+        }
+    }
+
+    public class Node<T>
+    {                
+        T[] data { get; set; }
+        Node<T>[] children;
+        Node<T> father;        
+
+        public Node(int order){
+            data = new T[order];
+            children = new Node<T>[order];            
+        }
+
+        public Node(int order, Node<T> father) {
+            data = new T[order];
+            children = new Node<T>[order];
+            this.father = father;
+
+        }
+
+        public void Sort()
+        {
+
+        }
+
+        internal bool isLeaf
+        {
+            get
+            {
+                return children[0] == null;
+            }
+        }
+
+        internal bool full
+        {
+            get
+            {
+                return children[children.Length - 1] != null;
+            }
+        }
+    }
+}
